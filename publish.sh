bb213eeb86d78a0951f49c56f43351cd6bca9d52
zngrn <vaibhavjhingran@gmail.com>
2022-10-20 15:44:48 +0200

    Adding gitgnore content

--- publish.sh
+++ publish.sh
@@ -1,1 +1,4 @@
+#!bin/sh
+
+# Deploy and publish script
 cd public && rm -rf .git && git init && git add . && git commit -m "Initial commit for zngrn.xyz site" && git branch -m master gh-pages && git remote add zngrn.xyz git@github.com:zngrn/blog.git && git push -f zngrn.xyz gh-pages
