fc046e7d9ab40df3dc4b1d0b6bcf3304d24b5033
zngrn <vaibhavjhingran@gmail.com>
2022-10-20 14:56:59 +0200

    Setup for publishing content

---
+++ publish.sh
@@ -0,0 +1,1 @@
+cd public && rm -rf .git && git init && git add . && git commit -m "Initial commit for zngrn.xyz site" && git branch -m master gh-pages && git remote add zngrn.xyz git@github.com:zngrn/blog.git && git push -f zngrn.xyz gh-pages
