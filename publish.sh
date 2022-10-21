a75db860b78903fd68658a8ad1cac6b246b16527
zngrn <vaibhavjhingran@gmail.com>
2022-10-21 13:12:40 +0200

    Fix publish script

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 #!bin/sh
 
 # Deploy and publish script
-cd public && rm -rf .git && git init && git add . && git commit -m "Publish site for zngrn.xyz" && git branch -m master gh-pages && git remote add origin git@github.com:zngrn/blog.git && git push -f origin gh-pages
+cd public && rm -rf .git && git init && git add . && git commit -m "Publish site for zngrn.xyz" && git branch -m master gh-pages && git remote add origin git@github.com-zngrn:zngrn/zngrn.xyz.git && git push -f origin gh-pages
